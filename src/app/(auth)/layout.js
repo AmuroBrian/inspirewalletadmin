8cc9cf43b0c4aa19b3589f1960e4929b0234060c
Brian Perez <perezbrian.sti@gmail.com>
2025-03-12 15:39:13 +0800

    First Commit

---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,19 @@
+import "./../globals.css";
+
+export const metadata = {
+    title: "Inspire Wallet Admin",
+    description: "Admin User Interface for Inspire Wallet",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body
+                className=""
+            >
+                <div className="w-full h-[100px] bg-amber-400"></div>
+                {children}
+            </body>
+        </html>
+    );
+}
---
+++ src/app/(dashboard)/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      Same
+      <Link href={"/main"}>Hello Click Me</Link>
+    </div>
+  );
+}
